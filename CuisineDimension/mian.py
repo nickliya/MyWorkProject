2e36ded840000ae16e9c496486c7d7474235d5de
yangqing <yq123456>
2017-11-14 17:18:42 +0800

    工具更新

--- CuisineDimension/mian.py
+++ CuisineDimension/mian.py
@@ -160,7 +160,7 @@ class Example(QtGui.QMainWindow):
         self.syText.setObjectName("syText")
         self.syText.setFixedHeight(390)
         self.syText.append(u"\n\n\n◆欢迎使用本工具！初次使用可以在此处查看使用帮助，有任何疑问和建议可以联系作者。")
-        self.syText.append(u"◆本工具所有美术资源或算法由萌百黑大@划破黑夜 破解，破解不易请珍惜成果，任何非法和损害他人利益行为与作者无关！")
+        self.syText.append(u"◆本工具所有美术资源或算法由萌百黑大@划破黑夜 破解，破解不易请珍惜成果。")
         self.syText.append(u"◆本工具旨在辅助玩家了解游戏信息，工具界面大致还原游戏界面是为了让玩家对工具产生亲切感。禁止窃取、泄露本工具美术资源，任何非法和损害他人利益行为与作者无关！")
         self.sygrid.addWidget(self.syText, 3, 0, 1, 4)
 
@@ -414,7 +414,6 @@ class Example(QtGui.QMainWindow):
         self.text.setFontPointSize(14)
         self.text.append(u"期待您的加入\n")
         self.text.setAlignment(QtCore.Qt.AlignCenter)
-
         self.rightgrid.addWidget(self.text, 0, 0)
         self.wigetIndex = [self.leftwiget, self.rightwiget]
 
