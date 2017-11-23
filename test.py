8fcc856a74a7e869e42ea3a86a851a459bba780c
yangqing <yq123456>
2017-11-23 17:29:32 +0800

    测试

--- test.py
+++ test.py
@@ -1,14 +1,42 @@
-# -*- coding:utf-8 -*-
+from PyQt4 import QtGui, QtCore
 
-import requests
 
-header = {"user-agent":"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.89 Safari/537.36"}
-payload = {
-    "action": "parse",
-    "prop": "text|images|links|externallinks|wikitext",
-    "page": "料理次元",
-    "format": "json"
-}
-r = requests.get("https://zh.moegirl.org/api.php", params=payload, headers=header)
-htmlData = r.json()
-print htmlData
+class Window(QtGui.QWidget):
+    def __init__(self, rows, columns):
+        QtGui.QWidget.__init__(self)
+        self.table = QtGui.QTableWidget(self)
+        self.table.setRowCount(rows)
+        self.table.setColumnCount(columns)
+        for column in range(columns):
+            for row in range(rows):
+                item = QtGui.QTableWidgetItem('Text%d' % row)
+                self.table.setItem(row, column, item)
+        self.edit = QtGui.QLineEdit(self)
+        self.button = QtGui.QPushButton('Search', self)
+        self.button.clicked.connect(self.handleButton)
+        layout = QtGui.QVBoxLayout(self)
+        layout.addWidget(self.table)
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+
+    def handleButton(self):
+        items = self.table.findItems(
+            self.edit.text(), QtCore.Qt.MatchContains)
+        if items:
+            for item in items:
+                print item.row()
+                for i in range(6):
+                    self.table.setRowHidden(i,True)
+                self.table.setRowHidden(item.row(),False)
+        else:
+            results = 'Found Nothing'
+            QtGui.QMessageBox.information(self, 'Search Results', results)
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    window = Window(6, 3)
+    window.resize(350, 300)
+    window.show()
+    sys.exit(app.exec_())
